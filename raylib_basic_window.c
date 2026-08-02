3b2499bf8796b4544d7adc60e6d3d8b43758f1a6
omark96 <43138339+omark96@users.noreply.github.com>
2026-08-03 00:01:17 +0200

    wip

--- raylib_basic_window.c
+++ raylib_basic_window.c
@@ -53,39 +53,28 @@ void corePrint(UmkaStackSlot *params, UmkaStackSlot *result)
     puts(msg);
 }
 
-void gameSave(UmkaStackSlot *params, UmkaStackSlot *result)
-{
-}
-
-void gameLoad(UmkaStackSlot *params, UmkaStackSlot *result)
-{
-}
-
 typedef struct GameApi
 {
     Umka *umka;
     UmkaFuncContext update;
     UmkaFuncContext init;
-    UmkaFuncContext serialize;
-    UmkaFuncContext deserialize;
-    UmkaFuncContext registerState;
+    UmkaFuncContext hotReload;
 } GameApi;
 
-GameApi initGame(char *name)
+void initGame(GameApi *gameApi, char *name)
 {
-    GameApi gameApi = {0};
-    gameApi.umka = umkaAlloc();
+    gameApi->umka = umkaAlloc();
     const UmkaType *stateType;
 
-    bool umkaOk = umkaInit(gameApi.umka, name, NULL, 1024 * 1024, NULL, 0, NULL, false, false, NULL);
+    bool umkaOk = umkaInit(gameApi->umka, name, NULL, 1024 * 1024, NULL, 0, NULL, false, false, NULL);
     if (umkaOk)
     {
-        umkaAddFunc(gameApi.umka, "drawText", &rlDrawText);
-        umkaAddFunc(gameApi.umka, "drawRectangle", &rlDrawRectangle);
-        umkaAddFunc(gameApi.umka, "getMouseX", &rlGetMouseX);
-        umkaAddFunc(gameApi.umka, "getMouseY", &rlGetMouseY);
-        umkaAddFunc(gameApi.umka, "isMouseButtonPressed", &rlIsMouseButtonPressed);
-        umkaAddFunc(gameApi.umka, "print", &corePrint);
+        umkaAddFunc(gameApi->umka, "drawText", &rlDrawText);
+        umkaAddFunc(gameApi->umka, "drawRectangle", &rlDrawRectangle);
+        umkaAddFunc(gameApi->umka, "getMouseX", &rlGetMouseX);
+        umkaAddFunc(gameApi->umka, "getMouseY", &rlGetMouseY);
+        umkaAddFunc(gameApi->umka, "isMouseButtonPressed", &rlIsMouseButtonPressed);
+        umkaAddFunc(gameApi->umka, "print", &corePrint);
         const char *umSourceNames[] = {"rl.um", "core.um"};
         const char *umSourceFiles[] = {
             (const char[]){
@@ -96,9 +85,9 @@ GameApi initGame(char *name)
                 , '\0'}};
         for (int i = 0; i < 2; i++)
         {
-            umkaAddModule(gameApi.umka, umSourceNames[i], umSourceFiles[i]);
+            umkaAddModule(gameApi->umka, umSourceNames[i], umSourceFiles[i]);
         }
-        umkaOk = umkaCompile(gameApi.umka);
+        umkaOk = umkaCompile(gameApi->umka);
 
         // umkaGetFunc(gameApi.umka, NULL, "registerState", &gameApi.registerState);
         // stateType = umkaGetParamType(gameApi.registerState.params, 0);
@@ -112,26 +101,207 @@ GameApi initGame(char *name)
     if (umkaOk)
     {
         printf("Umka initialized\n");
-        umkaGetFunc(gameApi.umka, NULL, "update", &gameApi.update);
-        umkaGetFunc(gameApi.umka, NULL, "init", &gameApi.init);
-        umkaGetFunc(gameApi.umka, NULL, "serialize", &gameApi.serialize);
-        umkaGetFunc(gameApi.umka, NULL, "deserialize", &gameApi.deserialize);
+        umkaGetFunc(gameApi->umka, NULL, "update", &gameApi->update);
+        umkaGetFunc(gameApi->umka, NULL, "init", &gameApi->init);
+        umkaGetFunc(gameApi->umka, NULL, "hotReload", &gameApi->hotReload);
     }
     else
     {
-        UmkaError *error = umkaGetError(gameApi.umka);
+        UmkaError *error = umkaGetError(gameApi->umka);
         printf("Umka error %s (%d, %d): %s\n", error->fileName, error->line, error->pos, error->msg);
     }
-    return gameApi;
+
+    umkaCall(gameApi->umka, &gameApi->init);
 }
 void onWarning(UmkaError *err)
 {
     fprintf(stderr, "%s (%s:%d): %s\n", err->fnName, err->fileName, err->line, err->msg);
 }
 
+void freeGame(GameApi *game)
+{
+    umkaFree(game->umka);
+    *game = (GameApi){0};
+}
+
+void transfer(Umka *dstUmka, void *dst, const UmkaType *dstType, Umka *srcUmka, void *src, const UmkaType *srcType)
+{
+    if (srcType->kind != dstType->kind)
+        return;
+
+    switch (srcType->kind)
+    {
+    case TYPE_BOOL:
+    case TYPE_CHAR:
+    case TYPE_INT8:
+    case TYPE_INT16:
+    case TYPE_INT32:
+    case TYPE_INT:
+    case TYPE_REAL32:
+    case TYPE_REAL:
+    case TYPE_UINT8:
+    case TYPE_UINT16:
+    case TYPE_UINT32:
+    case TYPE_UINT:
+    {
+        memcpy(dst, src, srcType->size);
+        break;
+    }
+    case TYPE_STRUCT:
+    {
+        for (int i = 0; i < srcType->numItems; i++)
+        {
+            const Field *srcField = srcType->field[i];
+            const Field *dstField = NULL;
+            for (int j = 0; j < dstType->numItems; j++)
+            {
+                if (strcmp(srcField->name, dstType->field[j]->name) == 0)
+                {
+                    dstField = dstType->field[j];
+                    break;
+                }
+            }
+            if (dstField)
+            {
+                transfer(dstUmka, dst + dstField->offset, dstField->type, srcUmka, src + srcField->offset, srcField->type);
+            }
+        }
+        break;
+    }
+    case TYPE_ARRAY:
+    {
+        const UmkaType *srcBase = umkaGetBaseType(srcType);
+        const UmkaType *dstBase = umkaGetBaseType(dstType);
+
+        if (srcBase->kind != dstBase->kind)
+            break;
+
+        const int count = srcType->numItems < dstType->numItems
+                              ? srcType->numItems
+                              : dstType->numItems;
+
+        const int64_t srcStride = srcBase->size;
+        const int64_t dstStride = dstBase->size;
+
+        for (int i = 0; i < count; i++)
+            transfer(dstUmka, dst + i * dstStride, dstBase,
+                     srcUmka, src + i * srcStride, srcBase);
+        break;
+    }
+
+    case TYPE_DYNARRAY:
+    {
+        typedef UmkaDynArray(void) DynArray;
+
+        const DynArray *srcArr = (const DynArray *)src;
+        DynArray *dstArr = (DynArray *)dst;
+
+        const int len = umkaGetDynArrayLen(srcArr);
+        umkaMakeDynArray(dstUmka, dstArr, dstType, len);
+
+        const UmkaType *srcBase = umkaGetBaseType(srcType);
+        const UmkaType *dstBase = umkaGetBaseType(dstType);
+
+        for (int i = 0; i < len; i++)
+            transfer(dstUmka, (void *)dstArr->data + i * dstArr->itemSize, dstBase,
+                     srcUmka, (void *)srcArr->data + i * srcArr->itemSize, srcBase);
+        break;
+    }
+    case TYPE_STR:
+    {
+        const char *srcStr = *(const char **)src;
+        char **dstSlot = (char **)dst;
+
+        if (*dstSlot)
+            umkaDecRef(dstUmka, *dstSlot);
+
+        *dstSlot = srcStr ? umkaMakeStr(dstUmka, srcStr) : NULL;
+        break;
+    }
+    case TYPE_PTR:
+    {
+        void *srcPtr = *(void **)src;
+        void **dstSlot = (void **)dst;
+
+        if (*dstSlot)
+        {
+            umkaDecRef(dstUmka, *dstSlot);
+        }
+
+        if (!srcPtr)
+        {
+            *dstSlot = NULL;
+            break;
+        }
+
+        const UmkaType *srcBase = umkaGetBaseType(srcType);
+        const UmkaType *dstBase = umkaGetBaseType(dstType);
+
+        if (srcBase->kind != dstBase->kind || dstBase->kind == TYPE_VOID)
+        {
+            *dstSlot = NULL;
+            break;
+        }
+
+        void *newObj = (dstBase->kind == TYPE_STRUCT || dstBase->kind == TYPE_ARRAY)
+                           ? umkaMakeStruct(dstUmka, dstBase)
+                           : umkaAllocData(dstUmka, dstBase->size, NULL);
+
+        *dstSlot = newObj;
+
+        transfer(dstUmka, (void *)newObj, dstBase,
+                 srcUmka, (void *)srcPtr, srcBase);
+
+        break;
+    }
+    case TYPE_MAP:
+    {
+        // WIP: Write a function for recursively copying over nodes from one struct to another.
+        const UmkaType *srcKeyType = srcType->base->field[2]->type;
+        const UmkaType *srcDataType = srcType->base->field[3]->type;
+        const UmkaType *srcKeyBaseType = umkaGetBaseType(srcKeyType);
+        const UmkaType *srcDataBaseType = umkaGetBaseType(srcDataType);
+
+        const UmkaType *dstKeyType = dstType->base->field[2]->type;
+        const UmkaType *dstDataType = dstType->base->field[3]->type;
+        const UmkaType *dstKeyBaseType = umkaGetBaseType(dstKeyType);
+        const UmkaType *dstDataBaseType = umkaGetBaseType(dstDataType);
+
+        if (srcKeyBaseType->kind != dstKeyBaseType->kind || srcDataBaseType->kind != dstDataBaseType->kind)
+        {
+            break;
+        }
+
+        UmkaMap *srcMap = src;
+        UmkaMap *dstMap = dst;
+        if (!dstMap)
+        {
+        }
+        // dstMap->type = dstType;
+
+        int a = 1;
+        break;
+    }
+    }
+}
+
+void hotReload(GameApi *curr, GameApi *next)
+{
+    umkaCall(curr->umka, &curr->hotReload);
+    void *currState = umkaGetResult(curr->hotReload.params, curr->hotReload.result)->ptrVal;
+    const UmkaType *currType = umkaGetResultType(curr->hotReload.params, curr->hotReload.result)->base;
+
+    umkaCall(next->umka, &next->hotReload);
+    void *nextState = umkaGetResult(next->hotReload.params, next->hotReload.result)->ptrVal;
+    const UmkaType *nextType = umkaGetResultType(next->hotReload.params, next->hotReload.result)->base;
+
+    transfer(next->umka, nextState, nextType, curr->umka, currState, currType);
+}
+
 int main()
 {
-    GameApi game = initGame("example.um");
+    GameApi game;
+    initGame(&game, "example.um");
 
     const int screenWidth = 800;
     const int screenHeight = 450;
@@ -150,33 +320,11 @@ int main()
 
         if (IsKeyPressed(KEY_F5))
         {
-            typedef UmkaDynArray(uint8_t) ByteArray;
-            ByteArray out = {0};
-            umkaGetResult(game.serialize.params, game.serialize.result)->ptrVal = &out;
-            umkaCall(game.umka, &game.serialize);
-            int length = umkaGetDynArrayLen(&out);
-
-            uint8_t *gameState = malloc(length * out.itemSize);
-            memcpy(gameState, out.data, length * out.itemSize);
-
-            umkaDecRef(game.umka, out.data);
-
-            umkaFree(game.umka);
-            game = initGame("example.um");
-            umkaCall(game.umka, &game.init);
-
-            ByteArray *in = (ByteArray *)umkaGetParam(game.deserialize.params, 0);
-            const UmkaType *arrayType = umkaGetParamType(game.deserialize.params, 0);
-
-            umkaMakeDynArray(game.umka, in, arrayType, length);
-            memcpy(in->data, gameState, length * in->itemSize);
-
-            if (umkaCall(game.umka, &game.deserialize) != 0)
-            {
-                UmkaError *err = umkaGetError(game.umka);
-            }
-
-            free(gameState);
+            GameApi next;
+            initGame(&next, "example.um");
+            hotReload(&game, &next);
+            freeGame(&game);
+            game = next;
         }
     }
     CloseWindow();
