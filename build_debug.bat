655052932459e59842b8a81945168a977831a247
omark96 <43138339+omark96@users.noreply.github.com>
2026-09-23 16:31:05 +0200

    wip

--- build_debug.bat
+++ build_debug.bat
@@ -1,3 +1,3 @@
 @REM  gcc -std=c23 -g -gdwarf-4 -O0 -DUMKA_STATIC -o raylib_basic_window_debug.exe ./src/main.c -Iinclude -Ivendor/umka/src -Lbuild/vendor -Llib -lraylib -lgdi32 -lwinmm -lumka
 
-gcc -std=c23 -g -O0 -DUMKA_STATIC -o main_debug.exe ./src/main.c -Iinclude -Ivendor/umka/src -Lbuild/vendor -Llib -lraylib -lgdi32 -lwinmm -lumka
+gcc -std=c23 -g -O0 -DUMKA_STATIC -o main_debug.exe ./src/main.c -Iinclude -Ivendor/umka/src -Lbuild/vendor/windows -lraylib -lgdi32 -lwinmm -lumka
--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-gcc -std=c23 -DUMKA_STATIC -o main.exe ./src/main.c -Iinclude -Ivendor/umka/src -Lbuild/vendor -Llib -lraylib -lgdi32 -lwinmm -lumka
+gcc -std=c23 -DUMKA_STATIC -o main.exe ./src/main.c -Iinclude -Ivendor/umka/src -Lbuild/vendor/windows -Llib -lraylib -lgdi32 -lwinmm -lumka
 main.exe
